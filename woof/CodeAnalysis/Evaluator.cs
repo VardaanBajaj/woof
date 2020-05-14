f6619d6f346db4a71b3ff63680488e40b16aaca5
Vardaan Bajaj <vardaanbajaj26@gmail.com>
2020-05-14 22:32:53 +0530

    Replace if-checks with switch in Evaluator.cs

--- woof/CodeAnalysis/Evaluator.cs
+++ woof/CodeAnalysis/Evaluator.cs
@@ -25,67 +25,92 @@ namespace woof.CodeAnalysis
         {
             if(node is BoundLiteralExpression n)
             {
-                return n.Value;
+                return EvaluateLiteralExpression(n);
             }
 
-            if(node is BoundVariableExpression v)
+            if (node is BoundVariableExpression v)
             {
-                return _variables[v.Variable];
+                return EvaluateVariableExpression(v);
             }
 
-            if(node is BoundAssignmentExpression a)
+            if (node is BoundAssignmentExpression a)
             {
-                var value = EvaluateExpression(a.Expression);
-                _variables[a.Variable] = value;
-                return value;
+                return EvaluateAssignmentExpression(a);
             }
 
-            if(node is BoundUnaryExpression u)
+            if (node is BoundUnaryExpression u)
             {
-                var operand = EvaluateExpression(u.Operand);
-                switch (u.Op.Kind)
-                {
-                    case BoundUnaryOperatorKind.Negation:
-                        return  -(int)operand;
-                    case BoundUnaryOperatorKind.Identity:
-                        return (int) operand;
-                    case BoundUnaryOperatorKind.LogicalNegation:
-                        return !(bool) operand;
-                    default:
-                        throw new Exception($"Unexpected unary operator {u.Op}");
-                }
+                return EvaluateUnaryExpression(u);
             }
 
-            if(node is BoundBinaryExpression b)
+            if (node is BoundBinaryExpression b)
             {
-                var left = EvaluateExpression(b.Left);
-                var right = EvaluateExpression(b.Right);
-
-                switch (b.Op.Kind)
-                {
-                    case BoundBinaryOperatorKind.Addition:
-                        return (int) left + (int) right;
-                    case BoundBinaryOperatorKind.Subtraction:
-                        return (int) left - (int) right;
-                    case BoundBinaryOperatorKind.Multiplication:
-                        return (int) left * (int) right;
-                    case BoundBinaryOperatorKind.Division:
-                        return (int) left / (int) right;
-                    case BoundBinaryOperatorKind.LogicalAnd:
-                        return (bool) left && (bool) right;
-                    case BoundBinaryOperatorKind.LogicalOr:
-                        return (bool) left || (bool) right;
-                    case BoundBinaryOperatorKind.Equals:
-                        return Equals(left, right);
-                    case BoundBinaryOperatorKind.NotEquals:
-                        return !Equals(left, right);
-                    default:
-                        throw new Exception($"Unexpected binary operator {b.Op}");
-                }
+                return EvaluateBinaryExpression(b);
 
             }
 
             throw new Exception($"Unexpected node {node.Kind}");
+        }
+
+        private object EvaluateBinaryExpression(BoundBinaryExpression b)
+        {
+            var left = EvaluateExpression(b.Left);
+            var right = EvaluateExpression(b.Right);
+
+            switch (b.Op.Kind)
+            {
+                case BoundBinaryOperatorKind.Addition:
+                    return (int)left + (int)right;
+                case BoundBinaryOperatorKind.Subtraction:
+                    return (int)left - (int)right;
+                case BoundBinaryOperatorKind.Multiplication:
+                    return (int)left * (int)right;
+                case BoundBinaryOperatorKind.Division:
+                    return (int)left / (int)right;
+                case BoundBinaryOperatorKind.LogicalAnd:
+                    return (bool)left && (bool)right;
+                case BoundBinaryOperatorKind.LogicalOr:
+                    return (bool)left || (bool)right;
+                case BoundBinaryOperatorKind.Equals:
+                    return Equals(left, right);
+                case BoundBinaryOperatorKind.NotEquals:
+                    return !Equals(left, right);
+                default:
+                    throw new Exception($"Unexpected binary operator {b.Op}");
+            }
+        }
+
+        private object EvaluateUnaryExpression(BoundUnaryExpression u)
+        {
+            var operand = EvaluateExpression(u.Operand);
+            switch (u.Op.Kind)
+            {
+                case BoundUnaryOperatorKind.Negation:
+                    return -(int)operand;
+                case BoundUnaryOperatorKind.Identity:
+                    return (int)operand;
+                case BoundUnaryOperatorKind.LogicalNegation:
+                    return !(bool)operand;
+                default:
+                    throw new Exception($"Unexpected unary operator {u.Op}");
+            }
+        }
+
+        private object EvaluateAssignmentExpression(BoundAssignmentExpression a)
+        {
+            var value = EvaluateExpression(a.Expression);
+            _variables[a.Variable] = value;
+            return value;
+        }
+
+        private object EvaluateVariableExpression(BoundVariableExpression v)
+        {
+            return _variables[v.Variable];
+        }
+
+        private static object EvaluateLiteralExpression(BoundLiteralExpression n)
+        {
+            return n.Value;
         }
     }
 }
